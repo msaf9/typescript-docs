bec8629b642d3db6065f10ddc204742d40c8d871
sahil afrid farookhi <msafarookhi@gmail.com>
2023-12-10 17:33:47 -0600

    Add Complete information about Types

--- docs/03_TYPES.md
+++ docs/03_TYPES.md
@@ -1,3 +1,29 @@
 # Types
 
 ## Introduction
+Different Data Types in TypeScript.
+
+## Code
+```typescript
+let thisIsAString: string = 'This is a String';
+let thisIsAnInteger: number = 20;
+let thisIsABoolean: boolean = true;
+enum thisIsAnENUM {
+    ENUM1 = 1,
+    ENUM2,
+    ENUM3
+};
+let enumValue: any = thisIsAnENUM[2];
+let thisIsAnArray: [string, number] = ['Value1', 10];
+let thisCanBeAny: any = 'This is any';
+
+function voidFunction(): void {
+    console.log('Void function returning nothing!');
+}
+```
+
+## Explanation
+Using console.log, the types are console logged.
+
+## Navigate to code
+> [variables.ts](../src/03_Types/types.ts)
--- src/03_Types/types.ts
+++ src/03_Types/types.ts
@@ -1,6 +1,27 @@
 // Different Types in TypeScript
-let thisIsString: string = 'This is a String';
-let thisIsInteger: number = 20;
+let thisIsAString: string = 'This is a String';
+let thisIsAnInteger: number = 20;
+let thisIsABoolean: boolean = true;
+enum thisIsAnENUM {
+    ENUM1 = 1,
+    ENUM2,
+    ENUM3
+};
+let enumValue: any = thisIsAnENUM[2];
+let thisIsAnArray: [string, number] = ['Value1', 10];
+let thisCanBeAny: any = 'This is any';
 
-console.log(thisIsString);
-console.log(thisIsInteger);
+function voidFunction(): void {
+    console.log('Void function returning nothing!');
+}
+
+console.log(thisIsAString);
+console.log(thisIsAnInteger);
+console.log(thisIsABoolean);
+console.log(thisIsAnArray);
+// Return ENUM number
+console.log(thisIsAnENUM.ENUM2);
+// Return ENUM value
+console.log(enumValue);
+console.log(thisCanBeAny);
+voidFunction();
